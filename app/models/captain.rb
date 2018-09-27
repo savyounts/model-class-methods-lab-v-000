e02bd25e3338fb2ae24c78d676a13ec3761f1921
Savannah Younts <savyounts@gmail.com>
2018-09-27 15:01:18 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,13 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    self.includes(boats: :classifications).where(classifications:{name: "Motorboat"}).uniq & self.sailors
+    mb = self.includes(boats: :classifications).where(classifications:{name: "Motorboat"}).uniq &
+    sailors =self.sailors
+    sailors.select do |sailor|
+      mbid = mb.collect {|m| m.id}
+      if mbid.include?(sailor.id)
+      end
+    end
   end
 
   def self.non_sailors
