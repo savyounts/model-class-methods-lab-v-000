d9a3fb021407b69b5b9414775c6121c35d4d921c
Savannah Younts <savyounts@gmail.com>
2018-09-27 14:54:51 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    self.includes(boats: :classifications).where(classifications:{name: "Motorboat"}).uniq & self.sailors
+  end
+
+  def self.non_sailors
+    includes(boats: :classifications).where.not(classifications:{name: "Sailboat"}).uniq
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,15 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    Classification.all
+  end
+
+  def self.longest
+    boats = Boat.arel_table
+    joins(classification: :boats).project(boats:[:length].maximum)
+
+  end
+
 end
